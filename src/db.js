33343882b19f185dcca88484ceb1d2e4ee12abf6
Tomislav-Troha <tomislavtroha69@gmail.com>
2021-05-27 13:11:32 +0200

    dodano spremanje u pojedinacnom planu za pojedinacnog usera

--- src/db.js
+++ src/db.js
@@ -1,36 +1,29 @@
-import mongo from "mongodb"
+import mongo from 'mongodb'
 
-
-let connection_string = "mongodb+srv://admin:admin@cluster0.wbrjl.mongodb.net/namirnice?retryWrites=true&w=majority";
-
-
-
-
+let connection_string =
+  'mongodb+srv://admin:admin@cluster0.wbrjl.mongodb.net/namirnice?retryWrites=true&w=majority'
 
 let client = new mongo.MongoClient(connection_string, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 
 let db = null
 
 export default () => {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    if (db && client.isConnected()) {
+      resolve(db)
+    }
 
-
-        if(db && client.isConnected()){
-            resolve(db)
-        }
-
-        client.connect(err => {
-            if(err) {
-                reject("Doslo je do greske " + err)
-            }
-            else {
-               // console.log("Uspijesno spajanje na bazu")
-                db = client.db("namirnice")
-                resolve(db)
-            }
-        })
+    client.connect(err => {
+      if (err) {
+        reject('Doslo je do greske ' + err)
+      } else {
+        // console.log("Uspijesno spajanje na bazu")
+        db = client.db('namirnice')
+        resolve(db)
+      }
     })
+  })
 }
--- src/index.js
+++ src/index.js
@@ -1,288 +1,240 @@
-import dotenv from "dotenv"
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
 
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
 import connect from './db.js'
 import data from './Namirnice.js'
 import mongo from 'mongodb'
-import auth from './auth.js';
+import auth from './auth.js'
 
- 
+const app = express() // instanciranje aplikacije
+const port = 3000 // port na kojem će web server slušati
 
-const app = express()  // instanciranje aplikacije
-const port = 3000  // port na kojem će web server slušati
-
-app.use(cors());
-app.use(express.json());
-
+app.use(cors())
+app.use(express.json())
 
 app.post('/', (req, res) => {
-    console.log("dobio sam post")
-    res.json({status: 'ok'})
+  console.log('dobio sam post')
+  res.json({ status: 'ok' })
 }),
+  app.get('/tajna', [auth.verify], (req, res) => {
+    res.json({ message: 'Ovo je tajna ' + req.jwt.email })
+  })
 
+app.post('/auth', async (req, res) => {
+  let user = req.body
 
-app.get("/tajna", [auth.verify] ,(req, res) => {
-
-    res.json({message: "Ovo je tajna " + req.jwt.email})
+  try {
+    let result = await auth.authUser(user.email, user.lozinka)
+    res.json(result)
+  } catch (e) {
+    return res.status(401).json({ error: e.message })
+  }
 })
 
-app.post("/auth", async (req, res) => {
-    let user = req.body
+app.post('/users', async (req, res) => {
+  let user = req.body
+  let id
 
-    try {
-       let result = await auth.authUser(user.email, user.lozinka )
-       res.json(result)
-    }
-    catch(e) {
-      return  res.status(401).json({error: e.message})
-
-    }
-})
-
-
-app.post("/users", async (req, res) => {
- 
-    let user = req.body;
-    let id;
-    
-
-    try{
+  try {
     id = await auth.registerUser(user)
-    
-    }
-    catch (e){
-      return res.status(500).json({error: e.message})
-    }
-    return res.json({id: id})
-    
+  } catch (e) {
+    return res.status(500).json({ error: e.message })
+  }
+  return res.json({ id: id })
 })
-
-
-
-
-
-
 
 //----------POJEDINACNI PLAN-----------//
 
+app.get('/pojedinacniPlan', [auth.verify], async (req, res) => {
+  let db = await connect()
 
-app.get('/pojedinacniPlan', [auth.verify] ,async (req, res) => {
-    let db = await connect()
+  let cursor = await db.collection('pojedinacniPlan').find()
+  let result = await cursor.toArray()
 
-    let cursor = await db.collection("pojedinacniPlan").find()
-    let result = await cursor.toArray()
-
-    res.json(result)
+  res.json(result)
 })
 
-app.get("/pojedinacniPlan/:id", async (req, res) => {
-    let id = req.params.id;
-    let db = await connect();
+app.get('/pojedinacniPlan/:email', [auth.verify], async (req, res) => {
+  let email = req.params.email
+  let db = await connect()
 
-    let doc = await db.collection("pojedinacniPlan").findOne({_id: mongo.ObjectID(id)})
-    res.json(doc)
+  let doc = await db.collection('pojedinacniPlan').findOne({ email: email })
+  res.json(doc)
+})
 
-   
+app.patch('/pojedinacniPlan/:email', [auth.verify], async (req, res) => {
+  let email = req.params.email
+  let data = req.body
 
-    });
+  let db = await connect()
 
-
-app.patch("/pojedinacniPlan/:id", [auth.verify] ,async (req, res) => {
-    let id = req.params.id
-    let data = req.body
-
-    let db = await connect();
-
-    let result = await db.collection("pojedinacniPlan").updateOne(
-        {_id: mongo.ObjectID(id) },
-        {
-            $set: data,
-        },
-        
-    )
-
-    if(result && result.modifiedCount == 1) {
-        let doc = await db.collection("pojedinacniPlan").findOne({_id:mongo.ObjectID(id)})
-        res.json(doc)
-    } else {
-        res.json({
-            status:"fail"
-        })
+  let result = await db.collection('pojedinacniPlan').updateOne(
+    { email: email },
+    {
+      $set: data
     }
+  )
+  //console.log(data, email)
+  if (result && result.modifiedCount == 1) {
+    let doc = await db
+      .collection('pojedinacniPlan')
+      .findOne({ email: email })
+    res.json(doc)
+  } else {
+    res.json({
+      status: 'fail'
+    })
+  }
 })
-
 
 //----------SpremiPoTjednu-----------//
 
+app.patch('/SpremiTjedan/:id', [auth.verify], async (req, res) => {
+  let id = req.params.id
+  let data = req.body
 
-app.patch("/SpremiTjedan/:id", [auth.verify] ,async (req, res) => {
-    
-    
-    let id = req.params.id;
-    let data = req.body;
-    
-    
+  let db = await connect()
 
-    let db = await connect();
-
-    let result = await db.collection("SpremiTjedan").updateOne(
-        { _id: mongo.ObjectId(id) },
-        {
-           $set: data,
-        }
-    )
-
-    if(result && result.modifiedCount == 1) {
-       let doc = await db.collection("SpremiTjedan").findOne({_id: mongo.ObjectID(id)})
-       res.json(doc)
-
-    } else {
-        res.json({
-            status:"fail"
-        })
+  let result = await db.collection('SpremiTjedan').updateOne(
+    { _id: mongo.ObjectId(id) },
+    {
+      $set: data
     }
+  )
 
-
-    });
-
-
-app.post("/SpremiTjedan",[auth.verify] , async (req, res) => {
-    let data = req.body
-
-    let db = await connect();
-
-    let result = await db.collection('SpremiTjedan').insertOne(data);
-
-    if(result && result.insertedCount == 1){
-        res.json(result.ops[0]);
-    } else {
-        res.json({
-            status:"fail"
-        })
-    }
-})
-
-app.get('/SpremiTjedan', [auth.verify] ,async (req, res) => {
-    let db = await connect()
-
-    let cursor = await db.collection("SpremiTjedan").find()
-    let result = await cursor.toArray()
-
-    res.json(result)
-})
-
-app.get("/SpremiTjedan/:id", [auth.verify] ,async (req, res) => {
-    let id = req.params.id;
-    let db = await connect();
-
-    let doc = await db.collection("SpremiTjedan").findOne({_id: mongo.ObjectID(id)})
+  if (result && result.modifiedCount == 1) {
+    let doc = await db
+      .collection('SpremiTjedan')
+      .findOne({ _id: mongo.ObjectID(id) })
     res.json(doc)
+  } else {
+    res.json({
+      status: 'fail'
+    })
+  }
+})
 
-   
+app.post('/SpremiTjedan', [auth.verify], async (req, res) => {
+  let data = req.body
 
-    });
+  let db = await connect()
 
+  let result = await db.collection('SpremiTjedan').insertOne(data)
 
+  if (result && result.insertedCount == 1) {
+    res.json(result.ops[0])
+  } else {
+    res.json({
+      status: 'fail'
+    })
+  }
+})
+
+app.get('/SpremiTjedan', [auth.verify], async (req, res) => {
+  let db = await connect()
+
+  let cursor = await db.collection('SpremiTjedan').find()
+  let result = await cursor.toArray()
+
+  res.json(result)
+})
+
+app.get('/SpremiTjedan/:id', [auth.verify], async (req, res) => {
+  let id = req.params.id
+  let db = await connect()
+
+  let doc = await db
+    .collection('SpremiTjedan')
+    .findOne({ _id: mongo.ObjectID(id) })
+  res.json(doc)
+})
 
 //------namirnice po id, mongo---------//
 
+app.get('/meso', async (req, res) => {
+  let db = await connect()
 
-app.get('/meso',async (req, res) => {
-    let db = await connect()
+  let cursor = await db.collection('meso').find()
+  let result = await cursor.toArray()
 
-    let cursor = await db.collection("meso").find()
-    let result = await cursor.toArray()
-
-    res.json(result)
+  res.json(result)
 })
 
 app.get('/kruh', async (req, res) => {
-    let db = await connect()
+  let db = await connect()
 
-    let cursor = await db.collection("kruh").find()
-    let result = await cursor.toArray()
+  let cursor = await db.collection('kruh').find()
+  let result = await cursor.toArray()
 
-    res.json(result)
+  res.json(result)
 })
 
 app.get('/ribe', async (req, res) => {
-    let db = await connect()
+  let db = await connect()
 
-    let cursor = await db.collection("ribe").find()
-    let result = await cursor.toArray()
+  let cursor = await db.collection('ribe').find()
+  let result = await cursor.toArray()
 
-    res.json(result)
+  res.json(result)
 })
 
 app.get('/brza_hrana', async (req, res) => {
-    let db = await connect()
+  let db = await connect()
 
-    let cursor = await db.collection("brza_hrana").find()
-    let result = await cursor.toArray()
+  let cursor = await db.collection('brza_hrana').find()
+  let result = await cursor.toArray()
 
-    res.json(result)
+  res.json(result)
 })
 
 app.get('/voce', async (req, res) => {
-    let db = await connect()
+  let db = await connect()
 
-    let cursor = await db.collection("voce").find()
-    let result = await cursor.toArray()
+  let cursor = await db.collection('voce').find()
+  let result = await cursor.toArray()
 
-    res.json(result)
+  res.json(result)
 })
 
 app.get('/povrce', async (req, res) => {
-    let db = await connect()
+  let db = await connect()
 
-    let cursor = await db.collection("povrce").find()
-    let result = await cursor.toArray()
+  let cursor = await db.collection('povrce').find()
+  let result = await cursor.toArray()
 
-    res.json(result)
+  res.json(result)
 })
 
 app.get('/mlijecni_proizvodi', async (req, res) => {
-    let db = await connect()
+  let db = await connect()
 
-    let cursor = await db.collection("mlijecni_proizvodi").find()
-    let result = await cursor.toArray()
+  let cursor = await db.collection('mlijecni_proizvodi').find()
+  let result = await cursor.toArray()
 
-    res.json(result)
+  res.json(result)
 })
-
-
-
 
 //namirnice po id
 
-app.get('/meso_memory', (req, res) => res.json(data.meso.data));
-app.get('/meso_memory/:id', (req, res) =>  {
+app.get('/meso_memory', (req, res) => res.json(data.meso.data))
+app.get('/meso_memory/:id', (req, res) => {
+  let id = req.params.id
+  console.log('ID od mesa ', id)
 
-    let id = req.params.id
-    console.log("ID od mesa ", id);
-
-    res.json(data.meso.data.filter((x) => x.id == id))
-
-});
+  res.json(data.meso.data.filter(x => x.id == id))
+})
 
 //ostale namirnice
-app.get('/kruh_memory', (req, res) => res.json(data.kruh));
+app.get('/kruh_memory', (req, res) => res.json(data.kruh))
 
-
-
-app.get('/ribe_memory', (req, res) => res.json(data.ribe));
-app.get('/brza_hrana_memory', (req, res) => res.json(data.brza_hrana));
-app.get('/voce_memory', (req, res) => res.json(data.voce));
-app.get('/povrce_memory', (req, res) => res.json(data.povrce));
-app.get('/mlijecni_proizvodi_memory', (req, res) => res.json(data.mlijecni_proizvodi));
-
-
-
-
-
-
-
+app.get('/ribe_memory', (req, res) => res.json(data.ribe))
+app.get('/brza_hrana_memory', (req, res) => res.json(data.brza_hrana))
+app.get('/voce_memory', (req, res) => res.json(data.voce))
+app.get('/povrce_memory', (req, res) => res.json(data.povrce))
+app.get('/mlijecni_proizvodi_memory', (req, res) =>
+  res.json(data.mlijecni_proizvodi)
+)
 
 app.listen(port, () => console.log(`Slušam na portu ${port}!`))
