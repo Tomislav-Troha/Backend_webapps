5c7649a371ac247854acbd10a4eeb0fabdd427e2
Tomislav-Troha <tomislavtroha69@gmail.com>
2021-05-31 15:54:20 +0200

    dodano za User Profil

--- src/auth.js
+++ src/auth.js
@@ -1,22 +1,22 @@
-import mongo from 'mongodb'
-import connect from './db.js'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-;(async () => {
-  let db = await connect()
-  await db.collection('users').createIndex({ email: 1 }, { unique: true })
-})()
+import mongo from "mongodb";
+import connect from "./db.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+(async () => {
+  let db = await connect();
+  await db.collection("users").createIndex({ email: 1 }, { unique: true });
+})();
 
 export default {
-  async registerUser (userData) {
-    let db = await connect()
+  async registerUser(userData) {
+    let db = await connect();
 
     let doc = {
       nadimak_obitelji: userData.nadimak_obitelji,
       broj_clanova: userData.broj_clanova,
       email: userData.email,
-      lozinka: await bcrypt.hash(userData.lozinka, 8)
-    }
+      lozinka: await bcrypt.hash(userData.lozinka, 8),
+    };
 
     let docPojedinacni = {
       email: userData.email,
@@ -25,8 +25,8 @@ export default {
       ciljMuski: userData.ciljMuski,
       ciljZene: userData.ciljZene,
       kalorijeMuski: userData.kalorijeMuski,
-      kalorijeZene: userData.kalorijeZene
-    }
+      kalorijeZene: userData.kalorijeZene,
+    };
 
     let docObiteljski = {
       email: userData.email,
@@ -47,64 +47,101 @@ export default {
       PErucak: userData.PErucak,
       PEvecera: userData.PEvecera,
       SUdorucak: userData.SUdorucak,
-      SUrucak:  userData.SUrucak,
+      SUrucak: userData.SUrucak,
       SUvecera: userData.SUvecera,
       Ndorucak: userData.Ndorucak,
       Nrucak: userData.Nrucak,
-      Nvecera: userData.Nvecera
+      Nvecera: userData.Nvecera,
+    };
 
-
-    }
+    let UserProfile = {
+      email: userData.email,
+      ime: userData.ime,
+      prezime: userData.prezime,
+      spol: userData.spol,
+      datumRodenja: userData.datumRodenja,
+      zanimanje: userData.zanimanje,
+    };
 
     try {
-      let result = await db.collection('users').insertOne(doc)
-      let resultPojedinacni = await db.collection('pojedinacniPlan').insertOne(docPojedinacni)
-      let resultObiteljski = await db.collection('SpremiTjedan').insertOne(docObiteljski)
+      let result = await db.collection("users").insertOne(doc);
+      let resultPojedinacni = await db
+        .collection("pojedinacniPlan")
+        .insertOne(docPojedinacni);
+      let resultObiteljski = await db
+        .collection("SpremiTjedan")
+        .insertOne(docObiteljski);
+      let resultUserProfile = await db
+        .collection("UserProfile")
+        .insertOne(UserProfile);
       if (result && result.insertedId) {
-        return result.insertedId
+        return result.insertedId;
       }
     } catch (e) {
-      if (e.name == 'MongoError' && e.code == 11000) {
-        throw new Error('Korisnik vec postoji')
+      if (e.name == "MongoError" && e.code == 11000) {
+        throw new Error("Korisnik vec postoji");
       }
     }
   },
 
-  async authUser (email, lozinka) {
-    let db = await connect()
+  async authUser(email, lozinka) {
+    let db = await connect();
 
-    let user = await db.collection('users').findOne({ email: email })
+    let user = await db.collection("users").findOne({ email: email });
 
     if (user && user.lozinka && (await bcrypt.compare(lozinka, user.lozinka))) {
-      delete user.lozinka
+      delete user.lozinka;
       let token = jwt.sign(user, process.env.JWT_SECRET, {
-        algorithm: 'HS512',
-        expiresIn: '1 week'
-      })
+        algorithm: "HS512",
+        expiresIn: "1 week",
+      });
       return {
         token,
-        email: user.email
-      }
+        email: user.email,
+      };
     } else {
-      throw new Error('Nista od prijave')
+      throw new Error("Nista od prijave");
     }
   },
 
-  verify (req, res, next) {
-    try {
-      let authorization = req.headers.authorization.split(' ')
-      let type = authorization[0]
-      let token = authorization[1]
+  async promjenaLozinke(email, old_password, new_password) {
+    let db = await connect();
+    let user = await db.collection("users").findOne({ email: email });
 
-      if (type !== 'Bearer') {
-        res.status(401).send()
-        return false
+    if (
+      user &&
+      user.lozinka &&
+      (await bcrypt.compare(old_password, user.lozinka))
+    ) {
+      let new_password_hashed = await bcrypt.hash(new_password, 8);
+
+      let result = await db.collection("users").updateOne(
+        { _id: user._id },
+        {
+          $set: {
+            lozinka: new_password_hashed,
+          },
+        }
+      );
+      return result.modifiedCount == 1;
+    }
+  },
+
+  verify(req, res, next) {
+    try {
+      let authorization = req.headers.authorization.split(" ");
+      let type = authorization[0];
+      let token = authorization[1];
+
+      if (type !== "Bearer") {
+        res.status(401).send();
+        return false;
       } else {
-        req.jwt = jwt.verify(token, process.env.JWT_SECRET)
-        return next()
+        req.jwt = jwt.verify(token, process.env.JWT_SECRET);
+        return next();
       }
     } catch (e) {
-      return res.status(401).send()
+      return res.status(401).send();
     }
-  }
-}
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,240 +1,299 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-import express from 'express'
-import cors from 'cors'
-import connect from './db.js'
-import data from './Namirnice.js'
-import mongo from 'mongodb'
-import auth from './auth.js'
+import express from "express";
+import cors from "cors";
+import connect from "./db.js";
+import data from "./Namirnice.js";
+import mongo from "mongodb";
+import auth from "./auth.js";
 
-const app = express() // instanciranje aplikacije
-const port = 3000 // port na kojem će web server slušati
+const app = express(); // instanciranje aplikacije
+const port = 3100; // port na kojem će web server slušati
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.post('/', (req, res) => {
-  console.log('dobio sam post')
-  res.json({ status: 'ok' })
+app.post("/", (req, res) => {
+  console.log("dobio sam post");
+  res.json({ status: "ok" });
 }),
-  app.get('/tajna', [auth.verify], (req, res) => {
-    res.json({ message: 'Ovo je tajna ' + req.jwt.email })
-  })
+  app.get("/tajna", [auth.verify], (req, res) => {
+    res.json({ message: "Ovo je tajna " + req.jwt.email });
+  });
 
-app.post('/auth', async (req, res) => {
-  let user = req.body
+//promjena lozinke
+
+app.patch("/users", [auth.verify], async (req, res) => {
+  let changes = req.body;
+
+  let email = req.jwt.email;
+
+  if (changes.new_password && changes.old_password) {
+    let result = await auth.promjenaLozinke(
+      email,
+      changes.old_password,
+      changes.new_password
+    );
+    if (result) {
+      res.status(201).send();
+    } else {
+      res.status(500).json({ error: "promjena lozinke neuspjesna" });
+    }
+  } else {
+    res.status(400).json({ error: "krivi upit" });
+  }
+});
+
+//autorizacija
+app.post("/auth", async (req, res) => {
+  let user = req.body;
 
   try {
-    let result = await auth.authUser(user.email, user.lozinka)
-    res.json(result)
+    let result = await auth.authUser(user.email, user.lozinka);
+    res.json(result);
   } catch (e) {
-    return res.status(401).json({ error: e.message })
+    return res.status(401).json({ error: e.message });
   }
-})
+});
 
-app.post('/users', async (req, res) => {
-  let user = req.body
-  let id
+app.post("/users", async (req, res) => {
+  let user = req.body;
+  let id;
 
   try {
-    id = await auth.registerUser(user)
+    id = await auth.registerUser(user);
   } catch (e) {
-    return res.status(500).json({ error: e.message })
+    return res.status(500).json({ error: e.message });
   }
-  return res.json({ id: id })
-})
+  return res.json({ id: id });
+});
+
+app.get("/users/:email", async (req, res) => {
+  let email = req.params.email;
+  let db = await connect();
+
+  let doc = await db.collection("users").findOne({ email: email });
+
+  res.json(doc);
+});
 
 //----------POJEDINACNI PLAN-----------//
 
-app.get('/pojedinacniPlan', [auth.verify], async (req, res) => {
-  let db = await connect()
+app.get("/pojedinacniPlan", [auth.verify], async (req, res) => {
+  let db = await connect();
 
-  let cursor = await db.collection('pojedinacniPlan').find()
-  let result = await cursor.toArray()
+  let cursor = await db.collection("pojedinacniPlan").find();
+  let result = await cursor.toArray();
 
-  res.json(result)
-})
+  res.json(result);
+});
 
-app.get('/pojedinacniPlan/:email', [auth.verify], async (req, res) => {
-  let email = req.params.email
-  let db = await connect()
+app.get("/pojedinacniPlan/:email", [auth.verify], async (req, res) => {
+  let email = req.params.email;
+  let db = await connect();
 
-  let doc = await db.collection('pojedinacniPlan').findOne({ email: email })
-  res.json(doc)
-})
+  let doc = await db.collection("pojedinacniPlan").findOne({ email: email });
+  res.json(doc);
+});
 
-app.patch('/pojedinacniPlan/:email', [auth.verify], async (req, res) => {
-  let email = req.params.email
-  let data = req.body
+app.patch("/pojedinacniPlan/:email", [auth.verify], async (req, res) => {
+  let email = req.params.email;
+  let data = req.body;
 
-  let db = await connect()
+  let db = await connect();
 
-  let result = await db.collection('pojedinacniPlan').updateOne(
+  let result = await db.collection("pojedinacniPlan").updateOne(
     { email: email },
     {
-      $set: data
+      $set: data,
     }
-  )
+  );
   //console.log(data, email)
   if (result && result.modifiedCount == 1) {
-    let doc = await db
-      .collection('pojedinacniPlan')
-      .findOne({ email: email })
-    res.json(doc)
+    let doc = await db.collection("pojedinacniPlan").findOne({ email: email });
+    res.json(doc);
   } else {
     res.json({
-      status: 'fail'
-    })
+      status: "fail",
+    });
   }
-})
+});
 
 //----------SpremiPoTjednu-----------//
 
-app.patch('/SpremiTjedan/:email', [auth.verify], async (req, res) => {
-  let email = req.params.email
-  let data = req.body
+app.patch("/SpremiTjedan/:email", [auth.verify], async (req, res) => {
+  let email = req.params.email;
+  let data = req.body;
 
-  let db = await connect()
+  let db = await connect();
 
-  let result = await db.collection('SpremiTjedan').updateOne(
+  let result = await db.collection("SpremiTjedan").updateOne(
     { email: email },
     {
-      $set: data
+      $set: data,
     }
-  )
- //console.log(data)
+  );
+  //console.log(data)
   if (result && result.modifiedCount == 1) {
-    let doc = await db
-      .collection('SpremiTjedan')
-      .findOne({ email: email })
-    res.json(doc)
+    let doc = await db.collection("SpremiTjedan").findOne({ email: email });
+    res.json(doc);
   } else {
     res.json({
-      status: 'fail'
-    })
+      status: "fail",
+    });
   }
-})
+});
 
-app.post('/SpremiTjedan', [auth.verify], async (req, res) => {
-  let data = req.body
+app.post("/SpremiTjedan", [auth.verify], async (req, res) => {
+  let data = req.body;
 
-  let db = await connect()
+  let db = await connect();
 
-  let result = await db.collection('SpremiTjedan').insertOne(data)
+  let result = await db.collection("SpremiTjedan").insertOne(data);
 
   if (result && result.insertedCount == 1) {
-    res.json(result.ops[0])
+    res.json(result.ops[0]);
   } else {
     res.json({
-      status: 'fail'
-    })
+      status: "fail",
+    });
   }
-})
+});
 
-app.get('/SpremiTjedan', [auth.verify], async (req, res) => {
-  let db = await connect()
+app.get("/SpremiTjedan", [auth.verify], async (req, res) => {
+  let db = await connect();
 
-  let cursor = await db.collection('SpremiTjedan').find()
-  let result = await cursor.toArray()
+  let cursor = await db.collection("SpremiTjedan").find();
+  let result = await cursor.toArray();
 
-  res.json(result)
-})
+  res.json(result);
+});
 
-app.get('/SpremiTjedan/:email', [auth.verify], async (req, res) => {
-  let email = req.params.email
-  let db = await connect()
+app.get("/SpremiTjedan/:email", [auth.verify], async (req, res) => {
+  let email = req.params.email;
+  let db = await connect();
 
-  let doc = await db
-    .collection('SpremiTjedan')
-    .findOne({ email: email })
-  res.json(doc)
-})
+  let doc = await db.collection("SpremiTjedan").findOne({ email: email });
+  res.json(doc);
+});
+
+//UserProfile
+
+app.post("/UserProfile/:email", [auth.verify], async (req, res) => {
+  let email = req.params.email;
+  let data = req.body;
+
+  let db = await connect();
+
+  let result = await db.collection("UserProfile").insert(
+    {
+      email: email,
+      ime: data.ime,
+      prezime: data.prezime,
+      spol: data.spol,
+      datumRodenja: data.datumRodenja,
+      zanimanje: data.zanimanje,
+    },
+    {
+      $set: data,
+    }
+  );
+  //console.log(data)
+  if (result && result.modifiedCount == 1) {
+    let doc = await db.collection("UserProfile").findOne({ email: email });
+    res.json(doc);
+  } else {
+    res.json({
+      status: "fail",
+    });
+  }
+});
 
 //------namirnice po id, mongo---------//
 
-app.get('/meso', async (req, res) => {
-  let db = await connect()
+app.get("/meso", async (req, res) => {
+  let db = await connect();
 
-  let cursor = await db.collection('meso').find()
-  let result = await cursor.toArray()
+  let cursor = await db.collection("meso").find();
+  let result = await cursor.toArray();
 
-  res.json(result)
-})
+  res.json(result);
+});
 
-app.get('/kruh', async (req, res) => {
-  let db = await connect()
+app.get("/kruh", async (req, res) => {
+  let db = await connect();
 
-  let cursor = await db.collection('kruh').find()
-  let result = await cursor.toArray()
+  let cursor = await db.collection("kruh").find();
+  let result = await cursor.toArray();
 
-  res.json(result)
-})
+  res.json(result);
+});
 
-app.get('/ribe', async (req, res) => {
-  let db = await connect()
+app.get("/ribe", async (req, res) => {
+  let db = await connect();
 
-  let cursor = await db.collection('ribe').find()
-  let result = await cursor.toArray()
+  let cursor = await db.collection("ribe").find();
+  let result = await cursor.toArray();
 
-  res.json(result)
-})
+  res.json(result);
+});
 
-app.get('/brza_hrana', async (req, res) => {
-  let db = await connect()
+app.get("/brza_hrana", async (req, res) => {
+  let db = await connect();
 
-  let cursor = await db.collection('brza_hrana').find()
-  let result = await cursor.toArray()
+  let cursor = await db.collection("brza_hrana").find();
+  let result = await cursor.toArray();
 
-  res.json(result)
-})
+  res.json(result);
+});
 
-app.get('/voce', async (req, res) => {
-  let db = await connect()
+app.get("/voce", async (req, res) => {
+  let db = await connect();
 
-  let cursor = await db.collection('voce').find()
-  let result = await cursor.toArray()
+  let cursor = await db.collection("voce").find();
+  let result = await cursor.toArray();
 
-  res.json(result)
-})
+  res.json(result);
+});
 
-app.get('/povrce', async (req, res) => {
-  let db = await connect()
+app.get("/povrce", async (req, res) => {
+  let db = await connect();
 
-  let cursor = await db.collection('povrce').find()
-  let result = await cursor.toArray()
+  let cursor = await db.collection("povrce").find();
+  let result = await cursor.toArray();
 
-  res.json(result)
-})
+  res.json(result);
+});
 
-app.get('/mlijecni_proizvodi', async (req, res) => {
-  let db = await connect()
+app.get("/mlijecni_proizvodi", async (req, res) => {
+  let db = await connect();
 
-  let cursor = await db.collection('mlijecni_proizvodi').find()
-  let result = await cursor.toArray()
+  let cursor = await db.collection("mlijecni_proizvodi").find();
+  let result = await cursor.toArray();
 
-  res.json(result)
-})
+  res.json(result);
+});
 
 //namirnice po id
 
-app.get('/meso_memory', (req, res) => res.json(data.meso.data))
-app.get('/meso_memory/:id', (req, res) => {
-  let id = req.params.id
-  console.log('ID od mesa ', id)
+app.get("/meso_memory", (req, res) => res.json(data.meso.data));
+app.get("/meso_memory/:id", (req, res) => {
+  let id = req.params.id;
+  console.log("ID od mesa ", id);
 
-  res.json(data.meso.data.filter(x => x.id == id))
-})
+  res.json(data.meso.data.filter((x) => x.id == id));
+});
 
 //ostale namirnice
-app.get('/kruh_memory', (req, res) => res.json(data.kruh))
+app.get("/kruh_memory", (req, res) => res.json(data.kruh));
 
-app.get('/ribe_memory', (req, res) => res.json(data.ribe))
-app.get('/brza_hrana_memory', (req, res) => res.json(data.brza_hrana))
-app.get('/voce_memory', (req, res) => res.json(data.voce))
-app.get('/povrce_memory', (req, res) => res.json(data.povrce))
-app.get('/mlijecni_proizvodi_memory', (req, res) =>
+app.get("/ribe_memory", (req, res) => res.json(data.ribe));
+app.get("/brza_hrana_memory", (req, res) => res.json(data.brza_hrana));
+app.get("/voce_memory", (req, res) => res.json(data.voce));
+app.get("/povrce_memory", (req, res) => res.json(data.povrce));
+app.get("/mlijecni_proizvodi_memory", (req, res) =>
   res.json(data.mlijecni_proizvodi)
-)
+);
 
-app.listen(port, () => console.log(`Slušam na portu ${port}!`))
+app.listen(port, () => console.log(`Slušam na portu ${port}!`));
